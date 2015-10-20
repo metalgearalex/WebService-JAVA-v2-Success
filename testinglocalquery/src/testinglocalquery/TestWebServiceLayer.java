f4890fdc63be77a02e4e1031160715d8e9bd4185
metalgearalex <alex.chavdaroff@gmail.com>
2015-10-19 20:42:35 -0400

    seems i'm successfully connecting to heroku. instead of an app's not
    working type message i'm getting a 404 message from heroku too (whereas
    before was just getting 404 from tomcat).
    
    about to try configuring it to run as a webapp-runner

--- testinglocalquery/src/testinglocalquery/TestWebServiceLayer.java
+++ testinglocalquery/src/testinglocalquery/TestWebServiceLayer.java
@@ -19,14 +19,14 @@ private static String connectionAnswer; //seems like this should be elsewhere or
     
     try { 
         Class.forName("org.postgresql.Driver"); //Before you can connect to a database, you need to load the driver. This is one of the 2 standard ways
-        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/RoomlooAndroidDummyDatabase", "postgres", "Metal123"); //username and password //used to get a Connection instance from JDBC; global JDBC method
+        connection = DriverManager.getConnection("jdbc:postgresql://ec2-54-227-253-238.compute-1.amazonaws.com:5432/d20hohh2uq3tib", "lqwgfavqzszazx", "dYfO-uBInG-WGgkqGFpz4GHAjc"); //username and password //used to get a Connection instance from JDBC; global JDBC method
         statement = connection.createStatement(); //Any time you want to issue SQL statements to the database, you require a Statement or PreparedStatement instance.
-        String querySQL = "SELECT price FROM apartmentinventoryv4"; //"SELECT attribute FROM tableName WHERE condition", Example: “SELECT price FROM ApartmentListDatabaseTable WHERE price < 4000”
+        String querySQL = "SELECT price FROM public.apartmentinventory"; //"SELECT attribute FROM tableName WHERE condition", Example: “SELECT price FROM ApartmentListDatabaseTable WHERE price < 4000”
         result = statement.executeQuery(querySQL); //result is an instance of ResultSet. executeQuery returns a ResultSet instance which contains the entire result
         //note that by default the Driver collects all the results for the query at once. You can make it fetch only a few rows however using cursors 
         connectionAnswer = "";
         while (result.next()) { //before reading any values you must call next() This returns true if there is a result, but more importantly, it prepares the row for processing.
-        	//working code to get console log >> System.out.println(result.getString("price"));
+        	//System.out.println(result.getString("price")); ////working code to get console log
         	//the code below also gets the result to show up in the console log
         	int price = result.getInt("price");
             connectionAnswer = connectionAnswer + price + " | " +  "\n";
